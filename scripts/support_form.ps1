97bd2b37b1571b2d8f58932b5d313a1ff203d63d
Mark Boeschen <50926327+firebytes@users.noreply.github.com>
2019-05-22 15:24:03 -0500

    Update support_form.ps1

--- scripts/support_form.ps1
+++ scripts/support_form.ps1
@@ -6,7 +6,7 @@
 - Create new "CMD" menu item under Device System Tray Menu in your Policy
 - Give it a title (I used 'New Support Ticket')
 - Option 1 - Automated - Insert this into the CMD line, replacing YOUR_DOMAIN_HERE with your own Syncro subdomain:
-powershell -ExecutionPolicy Bypass "Set-Variable -Name "subdomain" -Value "YOUR_DOMAIN_HERE"; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/MHCDatacomm/SyncroSupportForm/master/support_form.ps1','C:\ProgramData\Syncro\live\scripts\support_form.ps1');C:\ProgramData\Syncro\live\scripts\support_form.ps1"
+powershell -ExecutionPolicy Bypass "Set-Variable -Name "subdomain" -Value "firebytes"; (New-Object System.Net.WebClient).DownloadFile('https://github.com/firebytes/syncromsp/raw/master/scripts/support_form.ps1','C:\ProgramData\Syncro\live\scripts\support_form.ps1');C:\ProgramData\Syncro\live\scripts\support_form.ps1"
 See here for screenshot https://prnt.sc/n6z0w3
 -- This will automatically download the script from this repo each time the menu open is clicked. I suggest hosting the script on your own server or repo, as this is in development.
 - Option 2 - Manual - Copy this script into the C:\ProgramData\Syncro\live\scripts folder, then insert this into the CMD line of the system tray menu option:
