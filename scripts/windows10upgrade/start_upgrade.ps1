3162626621b5a8c27bb85fd8bb97e8c9317a68c3
Mark Boeschen <50926327+firebytes@users.noreply.github.com>
2019-05-24 18:09:49 -0500

    comments and multiple improvements

--- scripts/windows10upgrade/start_upgrade.ps1
+++ scripts/windows10upgrade/start_upgrade.ps1
@@ -5,108 +5,115 @@ $TicketValue = Create-Syncro-Ticket -Subdomain $subdomain -Subject "Windows 10 U
 $FileURL="https://s3.wasabisys.com/firebytes-public/Windows1903.iso"
 $FileOutput="C:\firebytes\Windows1903.iso"
 
-function downloadFile($url, $targetFile)
-{
-    "Downloading $url"
-    $uri = New-Object "System.Uri" "$url"
-    $request = [System.Net.HttpWebRequest]::Create($uri)
-    $request.set_Timeout(15000) #15 second timeout
-    $response = $request.GetResponse()
-    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-    $responseStream = $response.GetResponseStream()
-    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
-    $buffer = new-object byte[] 10KB
-    $count = $responseStream.Read($buffer,0,$buffer.length)
-    $downloadedBytes = $count
-    while ($count -gt 0)
-    {
-        [System.Console]::CursorLeft = 0
-        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength)
-        $targetStream.Write($buffer, 0, $count)
-        $count = $responseStream.Read($buffer,0,$buffer.length)
-        $downloadedBytes = $downloadedBytes + $count
-    }
-    "Finished Download"
-    Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "Finished Download" -Hidden $True -DoNotEmail $True
+$StartTime = Get-Date
 
-    $targetStream.Flush()
-    $targetStream.Close()
-    $targetStream.Dispose()
-    $responseStream.Dispose()
-}
 
 if (-not (Test-Path $FileOutput)) {
-  downloadFile $FileURL $FileOutput
+  # TICKET UPDATE - STARTING DOWNLOAD
+  Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "File Doesn't Exist - Starting Download" -Hidden $True -DoNotEmail $True
+
+  #Download File
+  (New-Object System.Net.WebClient).downloadFile($FileURL,$FileOutput)
+
+  # TICKET UPDATE - File Downloaded
+  Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "Finished Download in $((Get-Date).Subtract($StartTime).Seconds) seconds." -Hidden $True -DoNotEmail $True
+
   Write-host "File downloaded"
-  Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "File Confirmed" -Hidden $True -DoNotEmail $True
-} else {
+}
+else {
   write-host "File already exists"
   Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "File already exists - do not download" -Hidden $True -DoNotEmail $True
 }
 
 
 
+# TEST IF OUTPUT FILE EXISTS
 if (Test-Path $FileOutput) {
-  # EXTRACT SOMEHOW
+  # IF THE PATH EXISTS
   write-host "file exists"
   Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "File" -Body "File already exists - go ahead" -Hidden $True -DoNotEmail $True
   write-host "mounting image"
   Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Image" -Body "Mounting Image" -Hidden $True -DoNotEmail $True
+  # MOUNTING FILE TO VIRTUAL DRIVE
   $Volume = Mount-DiskImage -ImagePath $FileOutput -passthru | Get-DiskImage | Get-Volume
   write-host $Volume.DriveLetter
 
-    #Create batchfile for upgrade on each computer
+  # START WINDOWS UPGRADE CHECK
   write-host "start check"
   Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Install" -Body "Starting Check" -Hidden $True -DoNotEmail $True
   $SetupPath = "$($Volume.DriveLetter):\setup.exe"
-  write-host $SetupPath
 
-  $SetupProcess = Start-Process `
-         -FilePath $SetupPath `
-         -ArgumentList "/auto Upgrade /quiet /Compat ScanOnly /DynamicUpdate enable" `
-         -RedirectStandardError c:\firebytes\win_error.log `
-         -RedirectStandardOutput c:\firebytes\win_output.log `
-         -Passthru `
-         -Wait
+  # IF SETUP PATH EXISTS
+  if (Test-Path $SetupPath) {
 
-  Write-Host $SetupProcess.ExitCode
-  if ($SetupProcess.ExitCode -ne -1047526896) {
-    switch ($SetupProcess.ExitCode) {
-      -104752904 {
-        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900208 - Compatibility Issue" -Hidden $True -DoNotEmail $True
+    write-host $SetupPath
+
+    $SetupProcess = Start-Process `
+           -FilePath $SetupPath `
+           -ArgumentList "/auto Upgrade /quiet /Compat ScanOnly /DynamicUpdate enable" `
+           -RedirectStandardError c:\firebytes\win_error.log `
+           -RedirectStandardOutput c:\firebytes\win_output.log `
+           -Passthru `
+           -Wait
+
+    Write-Host $SetupProcess.ExitCode
+
+    # IF ERROR CODE
+    if ($SetupProcess.ExitCode -ne -1047526896) {
+      switch ($SetupProcess.ExitCode) {
+        -1047526904 {
+          Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900208 - Compatibility Issue" -Hidden $True -DoNotEmail $True
+        }
+        -1047526908 {
+          Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900204 - Migration Choice not available" -Hidden $True -DoNotEmail $True
+        }
+        -1047526912 {
+          Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900200 - Not eligible for Windows 10" -Hidden $True -DoNotEmail $True
+        }
+        -1047526898 {
+          Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC190020E - Not enough free space" -Hidden $True -DoNotEmail $True
+        }
+        default {
+          Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body $SetupProcess.ExitCode -Hidden $True -DoNotEmail $True
+        }
       }
-      -104752908 {
-        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900204 - Migration Choice not available" -Hidden $True -DoNotEmail $True
-      }
-      -104752912 {
-        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC1900200 - Not eligible for Windows 10" -Hidden $True -DoNotEmail $True
-      }
-      -104752898 {
-        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "0xC190020E - Not enough free space" -Hidden $True -DoNotEmail $True
-      }
-      default {
-        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body $SetupProcess.ExitCode -Hidden $True -DoNotEmail $True
-      }
+      write-host "error Code: $($SetupProcess.ExitCode)"
+      '{0:X}' -f $SetupProcess.ExitCode
+      dismount-diskimage -ImagePath $FileOutput
+      Exit 1
     }
-    write-host "error Code: $($SetupProcess.ExitCode)"
-    '{0:X}' -f $SetupProcess.ExitCode
-    dismount-diskimage -ImagePath $FileOutput
-    Exit 1
-  } else {
-    write-host "RUN SETUP NOW"
-    Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Install" -Body "Run Setup" -Hidden $True -DoNotEmail $True
+    # IF NO ERROR CODE
+    else {
+      write-host "RUN SETUP NOW"
+      Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Install" -Body "Create Scheduled Task" -Hidden $True -DoNotEmail $True
 
-    #$InstallProcess = Start-Process `
-    #       -FilePath $SetupPath `
-    #       -ArgumentList "/auto Upgrade /quiet /Compat IgnoreWarning /DynamicUpdate disable" `
-    #       -RedirectStandardError c:\firebytes\win_error.log `
-    #       -RedirectStandardOutput c:\firebytes\win_output.log
+      $now=Get-Date
+      $OneMinute=$now.AddSeconds(60)
+      $Trigger = New-ScheduledTaskTrigger -Once -At $OneMinute
+      $Settings = New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -DeleteExpiredTaskAfter "2 Days"
+      $User = "NT AUTHORITY\SYSTEM"
+      $Argument = "/auto Upgrade /quiet /Compat ScanOnly /DynamicUpdate enable"
+      $Action = New-ScheduledTaskAction -Execute $SetupPath -Argument $Argument
+
+      # IF SCHEDULED TASK IS CREATED
+      if (Register-ScheduledTask -TaskName "Upgrade Windows 10" -Trigger $Trigger -User $User -Action $Action -Settings $Settings -RunLevel Highest -Force) {
+        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Install" -Body "Scheduled Task Created" -Hidden $True -DoNotEmail $True
+      }
+      # IF SCHEDULED TASK IS NOT CREATED
+      else {
+        Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "Scheduled Task Not Created $($error[0])" -Hidden $True -DoNotEmail $True
+        Exit 1
+      }
+    } # END OF NO ERROR CODE
   }
-
-#CHECK RESULT???
-
-
-###########################################
-} else {
+  # IF SETUP PATH DOES NOT EXIST
+  else {
+    Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "Setup path of $($SetupPath) does not exist." -Hidden $True -DoNotEmail $True
+    Exit 1
+  }
+}
+else {
   write-host "file not downloaded correctly"
+  Create-Syncro-Ticket-Comment -Subdomain $subdomain -TicketIdOrNumber $TicketValue.ticket.id -Subject "Failed" -Body "File not downloaded correctly" -Hidden $True -DoNotEmail $True
+  Exit 1
 }
